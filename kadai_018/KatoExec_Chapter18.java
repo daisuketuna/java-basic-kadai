c7322f24b48134bb2c5d5eb6a21cd5e0f4410461
daisuketuna <daisoule@gmail.com>
2024-09-08 08:36:33 +0900

    課題提出

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,25 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+	public static void main(String[] args) {
+		
+		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+		taro.setGivenName("太郎");
+		taro.commonIntroduce();
+		taro.eachIntroduce();
+		
+		KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+		ichiro.setGivenName("一郎");
+		ichiro.commonIntroduce();
+		ichiro.eachIntroduce();
+		
+		KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+		hanako.setGivenName("花子");
+		hanako.commonIntroduce();
+		hanako.eachIntroduce();
+		
+		
+	}
+	
+	
+}
---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,18 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18{
+	public static void main(String[] args) {
+		
+		
+	}
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+	
+	public void eachIntroduce() {
+		System.out.println("私はJavaが得意です");
+	}
+
+	
+	
+	}
